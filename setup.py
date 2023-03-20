916887e736ef51045e06f28fc19ec686537cd3ed
NicKoehler <grillinicola@proton.me>
2023-03-20 14:38:06 +0100

    fix install script

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-
 setup(
     name="brawlhalla_api",
     version="0.0.1",
-    packages=find_packages(exclude="tests"),
+    packages=find_packages(exclude=["tests"]),
     description="Unofficial Brawlhalla API implementation",
     author="NicKoehler",
     author_email="grillinicola@proton.me",
@@ -16,6 +15,5 @@ setup(
     install_requires=[
         "httpx==0.23.3",
         "requests==2.28.2",
-        "python-dotenv==1.0.0",
     ],
 )
