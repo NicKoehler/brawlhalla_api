eb0569951a776435499a7ff1fae9d887355daae4
NicKoehler <grillinicola@proton.me>
2023-03-20 21:16:24 +0100

    fix glory calculation

--- brawlhalla_api/types/player_ranked.py
+++ brawlhalla_api/types/player_ranked.py
@@ -19,6 +19,7 @@ class PlayerRanked(Base):
         global_rank: int = None,
         region_rank: int = None,
         legends: PlayerRankedLegend = [],
+        rotating_ranked: RankingResult = None,
         **kwargs
     ) -> None:
         self.name = name
@@ -33,18 +34,31 @@ class PlayerRanked(Base):
         self.region_rank = region_rank
         self.legends = [PlayerRankedLegend(brawlhalla, **legend) for legend in legends]
         self.teams = [RankingResult(brawlhalla, **team) for team in kwargs["2v2"]]
+        if isinstance(rotating_ranked, list):
+            self.rotating_ranked = None
+        else:
+            self.rotating_ranked = RankingResult(brawlhalla, **rotating_ranked)
         self.estimated_glory = self._get_glory()
         self.estimated_elo_reset = utils.get_personal_elo_from_old_elo(self.rating)
         super().__init__(brawlhalla)
 
     def _get_glory(self) -> int:
-        peak_rating = self.peak_rating
         total_wins = self.wins
+        peak_rating = self.peak_rating
 
-        for elem in self.teams + self.legends:
+        for elem in self.teams:
             total_wins += elem.wins
             if elem.peak_rating > peak_rating:
                 peak_rating = elem.peak_rating
+
+        for elem in self.legends:
+            if elem.peak_rating > peak_rating:
+                peak_rating = elem.peak_rating
+
+        if self.rotating_ranked:
+            total_wins += self.rotating_ranked.wins
+            if self.rotating_ranked.peak_rating > peak_rating:
+                peak_rating = self.rotating_ranked.peak_rating
 
         glory_wins = utils.get_glory_from_wins(total_wins)
         glory_rating = utils.get_glory_from_best_rating(peak_rating)
