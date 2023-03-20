f103bca1cfd524c9d0677531100ea34b037bd7fe
NicKoehler <grillinicola@proton.me>
2023-03-20 16:56:28 +0100

    added estimated_glory and estimated_elo_reset to PlayerRanked

--- brawlhalla_api/types/player_ranked.py
+++ brawlhalla_api/types/player_ranked.py
@@ -1,6 +1,7 @@
 from .base import Base
 from .ranking_result import RankingResult
 from .player_legend import PlayerRankedLegend
+from .. import utils
 
 
 class PlayerRanked(Base):
@@ -32,4 +33,20 @@ class PlayerRanked(Base):
         self.region_rank = region_rank
         self.legends = [PlayerRankedLegend(brawlhalla, **legend) for legend in legends]
         self.teams = [RankingResult(brawlhalla, **team) for team in kwargs["2v2"]]
+        self.estimated_glory = self._get_glory()
+        self.estimated_elo_reset = utils.get_personal_elo_from_old_elo(self.rating)
         super().__init__(brawlhalla)
+
+    def _get_glory(self) -> int:
+        peak_rating = self.peak_rating
+        total_wins = self.wins
+
+        for elem in self.teams + self.legends:
+            total_wins += elem.wins
+            if elem.peak_rating > peak_rating:
+                peak_rating = elem.peak_rating
+
+        glory_wins = utils.get_glory_from_wins(total_wins)
+        glory_rating = utils.get_glory_from_best_rating(peak_rating)
+
+        return glory_rating + glory_wins
