ae75b8c74a5097ac32eb6e11d0f3e84784417b37
NicKoehler <grillinicola@proton.me>
2023-04-11 16:20:28 +0200

    updated legends docstrings

--- brawlhalla_api/types/legends.py
+++ brawlhalla_api/types/legends.py
@@ -1,18 +1,10 @@
 """
 This module provides two dataclass, Legend and LegendDetails.
-Legend represents a Brawlhalla Legend and contains basic information such as:
-name, weapons, stats etc.
-
-LegendDetails is a subclass of Legend and contains additional information such as
-the legend's bio, quotes etc.
 
 Both classes take a Brawlhalla instance as a parameter
 and can be instantiated with optional keyword arguments that
 correspond to the legend's attributes. If an attribute is not provided,
 it defaults to None.
-
-Note: These classes require the brawlhalla_api module to be imported,
-otherwise a NameError will be raised.
 """
 
 from __future__ import annotations
@@ -25,6 +17,23 @@ if TYPE_CHECKING:
 
 @dataclass
 class Legend:
+    """
+    Legend represents a Brawlhalla Legend and contains basic information.
+
+    brawlhalla: an instance of the Brawlhalla class
+    legend_id: the legend's unique ID
+    legend_name_key: the legend's name key
+    bio_name: the legend's bio name
+    bio_aka: the legend's bio aka
+    weapon_one: the legend's first weapon
+    weapon_two: the legend's second weapon
+    strength: the legend's strength
+    dexterity: the legend's dexterity
+    defense: the legend's defense
+    speed: the legend's speed
+
+    """
+
     def __init__(
         self,
         brawlhalla: Brawlhalla,
@@ -45,6 +54,19 @@ class Legend:
 
 @dataclass
 class LegendDetails(Legend):
+    """
+
+    LegendDetails is a subclass of Legend and contains additional information.
+
+    bio_quote: the legend's quote
+    bio_quote_about_attrib: the legend's quote about attrib
+    bio_quote_from: the legend's quote from
+    bio_quote_from_attrib: the legend's quote from attrib
+    bio_text: the legend's quote
+    bot_name: the legend's bot name
+
+    """
+
     def __init__(
         self,
         brawlhalla: Brawlhalla,
