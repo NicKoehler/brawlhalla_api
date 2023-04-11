333a1843588e73e5a7ad64fb8c6e949b5f551f6d
NicKoehler <grillinicola@proton.me>
2023-04-11 10:59:04 +0200

    added module docstring, simplify functions

--- brawlhalla_api/utils.py
+++ brawlhalla_api/utils.py
@@ -1,4 +1,9 @@
-import math
+"""
+This module contains utility functions for calculating
+the glory of a player and getting the elo reset.
+"""
+
+from math import floor, log10
 
 
 def get_glory_from_wins(total_wins: int) -> int:
@@ -11,7 +16,7 @@ def get_glory_from_wins(total_wins: int) -> int:
     """
     if total_wins <= 150:
         return 20 * total_wins
-    return math.floor((10 * (45 * (math.log10(total_wins * 2) ** 2))) + 245)
+    return floor((10 * (45 * (log10(total_wins * 2) ** 2))) + 245)
 
 
 def get_glory_from_best_rating(best_rating: int) -> int:
@@ -25,19 +30,19 @@ def get_glory_from_best_rating(best_rating: int) -> int:
     retval = 0
     if best_rating < 1200:
         retval = 250
-    elif best_rating >= 1200 and best_rating < 1286:
+    elif 1200 <= best_rating < 1286:
         retval = 10 * (25 + (0.872093023 * (86 - (1286 - best_rating))))
-    elif best_rating >= 1286 and best_rating < 1390:
+    elif 1286 <= best_rating < 1390:
         retval = 10 * (100 + (0.721153846 * (104 - (1390 - best_rating))))
-    elif best_rating >= 1390 and best_rating < 1680:
+    elif 1390 <= best_rating < 1680:
         retval = 10 * (187 + (0.389655172 * (290 - (1680 - best_rating))))
-    elif best_rating >= 1680 and best_rating < 2000:
+    elif 1680 <= best_rating < 2000:
         retval = 10 * (300 + (0.428125 * (320 - (2000 - best_rating))))
-    elif best_rating >= 2000 and best_rating < 2300:
+    elif 2000 <= best_rating < 2300:
         retval = 10 * (437 + (0.143333333 * (300 - (2300 - best_rating))))
     elif best_rating >= 2300:
         retval = 10 * (480 + (0.05 * (400 - (2700 - best_rating))))
-    return math.floor(retval)
+    return floor(retval)
 
 
 def get_hero_elo_from_old_elo(old_elo: int) -> int:
@@ -49,8 +54,8 @@ def get_hero_elo_from_old_elo(old_elo: int) -> int:
 
     """
     if old_elo < 2000:
-        return math.floor((old_elo + 375) / 1.5)
-    return math.floor(1583 + (old_elo - 2000) / 10)
+        return floor((old_elo + 375) / 1.5)
+    return floor(1583 + (old_elo - 2000) / 10)
 
 
 def get_personal_elo_from_old_elo(old_elo: int) -> int:
@@ -61,5 +66,5 @@ def get_personal_elo_from_old_elo(old_elo: int) -> int:
     :return: The personal Elo rating of the player after the calculation.
     """
     if old_elo >= 1400:
-        return math.floor(1400 + (old_elo - 1400.0) / (3.0 - (3000 - old_elo) / 800.0))
+        return floor(1400 + (old_elo - 1400.0) / (3.0 - (3000 - old_elo) / 800.0))
     return old_elo
